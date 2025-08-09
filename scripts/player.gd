dc01a0f558ca5f004111b602be609c7742ac5005
Jonatan Gezelius <jonatan.gezelius@gmail.com>
2025-08-09 23:18:46 +0200

    Add more animation. Adjust hitboxes.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,26 @@
 extends CharacterBody2D
 
-@export var speed := 200
+@export var speed := 100
+
+@onready
+var animation: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var dir = Vector2.ZERO
 	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	velocity = dir.normalized() * speed
+	velocity.y /= 2
+	
+	if dir.x > 0:
+		animation.flip_h = false
+	elif dir.x < 0:
+		animation.flip_h = true
+	
+	if dir.x == 0:
+		animation.play("idleSouth")
+	else:
+		animation.play("runEast")
 	move_and_slide()
 
 func teleport(position: Vector2):
