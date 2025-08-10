4200687c44538a26ab4fb519ac81495b0d2ce234
Jonatan Gezelius <jonatan.gezelius@gmail.com>
2025-08-10 21:12:25 +0200

    Add more animations for player.

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,9 @@ extends CharacterBody2D
 @onready
 var animation: AnimatedSprite2D = $AnimatedSprite2D
 
+enum AnimationDirection { EAST, WEST, NORTH, SOUTH }
+var lastDirection: AnimationDirection = AnimationDirection.SOUTH
+
 func _physics_process(delta):
 	var dir = Vector2.ZERO
 	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -17,10 +20,23 @@ func _physics_process(delta):
 	elif dir.x < 0:
 		animation.flip_h = true
 	
-	if dir.x == 0:
-		animation.play("idleSouth")
+	if dir.x == 0 and dir.y == 0:
+		match lastDirection:
+			AnimationDirection.NORTH:
+				animation.play("idleNorth")
+			AnimationDirection.SOUTH:
+				animation.play("idleSouth")
+			_:
+				animation.play("idleEast")
+	elif dir.y < -abs(dir.x):
+		animation.play("runNorth")
+		lastDirection = AnimationDirection.NORTH
+	elif dir.y > abs(dir.x):
+		animation.play("runSouth")
+		lastDirection = AnimationDirection.SOUTH
 	else:
 		animation.play("runEast")
+		lastDirection = AnimationDirection.EAST
 	move_and_slide()
 
 func teleport(position: Vector2):
